25203bcce48a84fe0dec5616fb758163277fe93d
shaheerjawaid <shaheerjawaid06@gmail.com>
2024-08-11 03:09:19 +0500

    Auth Routes and Controller are Connected

--- api/routes/auth.routes.js
+++ api/routes/auth.routes.js
@@ -1,24 +1,11 @@
 const { Router } = require('express');
+const { signup, verifyEmail, verifyPassword, forgotPassword, resetPassword } = require('../controllers/auth.controller');
 const router = Router();
 
-router.post('/auth/signup', (request, response) => {
-    response.status(200).json({ message: "SignUp API Working" });
-});
-
-router.post('/auth/verify-email', (request, response) => {
-    response.status(200).json({ message: "Verify Email API Working" });
-});
-
-router.post('/auth/verify-password', (request, response) => {
-    response.status(200).json({ message: "Verify Password API Working" });
-});
-
-router.post('/auth/forgot-password', (request, response) => {
-    response.status(200).json({ message: "Forgot Password API Working" });
-});
-
-router.post('/auth/reset-password/:token', (request, response) => {
-    response.status(200).json({ message: "Reset Password API Working" });
-});
+router.post('/auth/signup', signup);
+router.post('/auth/verify-email', verifyEmail);
+router.post('/auth/verify-password', verifyPassword);
+router.post('/auth/forgot-password', forgotPassword);
+router.post('/auth/reset-password/:token', resetPassword);
 
 module.exports = router;
